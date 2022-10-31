7b3ef1cff0387de508b7bad3f4ff11a423bdb907
AJ <necessarylion@gmail.com>
2022-10-31 23:52:18 +0700

    show failed reason on failed template

--- views/opn-failed.blade.php
+++ views/opn-failed.blade.php
@@ -26,6 +26,12 @@
                     d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" />
             </svg>
             <h2>Failed to make payment</h2>
+
+            @if(!empty($charge->failure_message))
+            <p class="secondary text-center">
+                Payment failed due to {{$charge->failure_message}}.
+            </p>
+            @endif
             
            
             <table class="table">
@@ -42,7 +48,7 @@
             </table>
             <br>
             <div class="failed-buttons">
-                <a href="{{$attempt['authorized_uri']}}" class="retry-button">Retry Payment</a>
+                <a href="{{$attempt['authorized_uri']}}" class="retry-button">Retry </a>
                 <a href="{{$attempt['redirect_uri']}}" class="failed-back-button">Back to Merchant</a>
             </div>
             <br>
